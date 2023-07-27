9149d5af01bec7ff7300647ce83a33c5c9d272b1
DreamingDevil <127566576+DreamingDevil@users.noreply.github.com>
2023-07-27 18:29:02 -0400

    last couple of class activity sessions

---
+++ 19.1/19.1-activities/01_Hello_World_Again/unsolved/js/index.js
@@ -0,0 +1,4 @@
+// Create a variable below
+var helloWorld = 'Hello World! I am dying internally and externally';
+// Console.log() your variable here.
+console.log(helloWorld);
---
+++ 19.2/Copy of 19.2-activities/01_Conditionals/unsolved/js/index.js
@@ -0,0 +1,43 @@
+$('.tab').on("click", function(){
+    
+    if ($(this).hasClass("tab1")) {
+        $(".tab1").addClass("active");
+        $(".tab2").removeClass("active");
+        $(".tab3").removeClass("active");
+        $(".tab4").removeClass("active");
+        $("#tabContent1").css("display", "block");
+        $("#tabContent2").css("display", "none");
+        $("#tabContent3").css("display", "none");
+        $("#tabContent4").css("display", "none");
+    }
+    else if ($(this).hasClass("tab2")) {
+        $(".tab2").addClass("active");
+        $(".tab1").removeClass("active");
+        $(".tab3").removeClass("active");
+        $(".tab4").removeClass("active");
+        $("#tabContent2").css("display", "block");
+        $("#tabContent1").css("display", "none");
+        $("#tabContent3").css("display", "none");
+        $("#tabContent4").css("display", "none");
+    }
+    else if ($(this).hasClass("tab3")) {
+        $(".tab3").addClass("active");
+        $(".tab1").removeClass("active");
+        $(".tab2").removeClass("active");
+        $(".tab4").removeClass("active");
+        $("#tabContent3").css("display", "block");
+        $("#tabContent1").css("display", "none");
+        $("#tabContent2").css("display", "none");
+        $("#tabContent4").css("display", "none");
+    }
+    else if ($(this).hasClass("tab4")) {
+        $(".tab4").addClass("active");
+        $(".tab1").removeClass("active");
+        $(".tab2").removeClass("active");
+        $(".tab3").removeClass("active");
+        $("#tabContent4").css("display", "block");
+        $("#tabContent1").css("display", "none");
+        $("#tabContent2").css("display", "none");
+        $("#tabContent3").css("display", "none");
+    }
+});
